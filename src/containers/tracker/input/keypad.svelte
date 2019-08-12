5df1cd45467d866ccd1e7894d842d1a9f37a2369
Brandon Corbin <brandon@icorbin.com>
2019-08-12 18:27:53 -0400

    Testing keypad as button

--- src/containers/tracker/input/keypad.svelte
+++ src/containers/tracker/input/keypad.svelte
@@ -115,7 +115,7 @@
       width: 280px;
     }
   }
-  div.button {
+  button.button {
     touch-action: manipulation;
     width: 64px;
     height: 64px;
@@ -167,7 +167,7 @@
         {#each key as button, bindex}
           <div class="col-3">
             {#if button !== null}
-              <div
+              <button
                 class="button button-{bindex}"
                 use:tap
                 on:tap={() => {
@@ -175,7 +175,7 @@
                   return false;
                 }}>
                 {button}
-              </div>
+              </button>
             {/if}
           </div>
         {/each}
