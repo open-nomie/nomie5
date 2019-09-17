eb1fb4840e4ed5276af6e87fdb609928c41d05ed
Brandon Corbin <brandon@icorbin.com>
2019-09-17 18:54:30 -0400

    Fixing borked release
    - Adding closing of splash screen in user if they don't have storage setup yet.

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,7 +16,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-4.2.19';
+const PRECACHE = 'precache-4.2.20';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
