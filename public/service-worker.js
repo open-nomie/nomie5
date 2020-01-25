a04cac451e1b05a73975a846889977f793ec8d5d
Brandon Corbin <brandon@icorbin.com>
2020-01-25 17:39:13 -0500

    Working on service workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  Copyright 2016 Google Inc. All Rights Reserved.
@@ -16,13 +16,13 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "precache-4.6.15";
+const PRECACHE = "precache-4.6.20";
 const RUNTIME = "runtime";
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  "index.html",
-  "./" // Alias for index.html
+  // "index.html",
+  // "./" // Alias for index.html
 ];
 
 // The install handler takes care of precaching the resources we always need.
@@ -62,7 +62,8 @@ self.addEventListener("activate", event => {
 // from the network before returning it to the page.
 self.addEventListener("fetch", event => {
   // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.search(`/api/`) > -1) ; else if (
+  if (event.request.url.search(`/api/`) > -1);
+  else if (
     event.request.url.startsWith(self.location.origin) &&
     self.location.origin.search("nomie") > -1
   ) {
