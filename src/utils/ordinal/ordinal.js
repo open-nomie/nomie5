4188ee9d8676cb179aae2c52a26803afe98c4e53
Brandon Corbin <brandon@icorbin.com>
2020-03-22 16:29:27 -0400

    Create ordinal.js

---
+++ src/utils/ordinal/ordinal.js
@@ -0,0 +1,18 @@
+const Ordinal = value => {
+  if (!value) return "";
+  let i = parseInt(value);
+  var j = i % 10,
+    k = i % 100;
+  if (j == 1 && k != 11) {
+    return i + "st";
+  }
+  if (j == 2 && k != 12) {
+    return i + "nd";
+  }
+  if (j == 3 && k != 13) {
+    return i + "rd";
+  }
+  return i + "th";
+};
+
+export { Ordinal };
