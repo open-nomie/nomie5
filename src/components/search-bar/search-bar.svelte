064beb61187d28e6c9b6175a262e9cf50994abf7
Brandon Corbin <brandon@icorbin.com>
2020-03-27 15:39:35 -0400

    Search bar improvements

--- src/components/search-bar/search-bar.svelte
+++ src/components/search-bar/search-bar.svelte
@@ -6,6 +6,7 @@
   const dispatch = createEventDispatcher();
 
   export let searchTerm = null;
+  export let autocomplete = false;
   // export let hasResults = false;
 
   // FIre off changes when input changes
@@ -33,10 +34,20 @@
 </script>
 
 <style lang="scss">
-  .search-bar {
+  :global(.search-bar) {
     position: relative;
-    input {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    .input-wrapper {
       border-radius: 6px;
+      background-color: var(--color-faded-1) !important;
+      flex-grow: 1;
+      flex-shrink: 1;
+      input {
+        border: none;
+        background-color: transparent;
+      }
     }
   }
   :global(.search-bar .btn-action-clear) {
@@ -53,15 +64,19 @@
       <i class="zmdi zmdi-close-circle text-sm" />
     </button>
   {/if}
-  <input
-    type="text"
-    class="search-input"
-    on:keyup={searchKeypress}
-    bind:value={searchTerm}
-    placeholder="{Lang.t('general.search')}..." />
-  {#if searchTerm}
-    <button class="btn btn-sm text-primary" on:click={fireSearch}>
-      Search
-    </button>
-  {/if}
+  <div class="input-wrapper n-row">
+    <slot name="left" />
+    <input
+      type="text"
+      class="search-input"
+      on:keyup={searchKeypress}
+      bind:value={searchTerm}
+      placeholder="{Lang.t('general.search')}..." />
+    <slot name="right" />
+    {#if searchTerm && !autocomplete}
+      <button class="btn btn-sm text-inverse-2" on:click={fireSearch}>
+        Search
+      </button>
+    {/if}
+  </div>
 </NToolbar>
