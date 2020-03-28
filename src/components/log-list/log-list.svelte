df96b8a9e13d8d58f9d8e02bd6c08b1272b23c1c
Brandon Corbin <brandon@icorbin.com>
2020-03-28 11:09:35 -0400

    Simplifying log-list component

--- src/components/log-list/log-list.svelte
+++ src/components/log-list/log-list.svelte
@@ -5,20 +5,10 @@
   import NSpinner from "../spinner/spinner.svelte";
 
   export let logs = null;
-  export let loading = false;
 </script>
 
 <div class="n-list">
-
   {#each logs as log}
     <LogItem {log} showMore={false} />
   {/each}
-  {#if loading}
-    <NItem className="py-2">
-      <div slot="left">
-        <NSpinner size={24} />
-      </div>
-      Finding More...
-    </NItem>
-  {/if}
 </div>
