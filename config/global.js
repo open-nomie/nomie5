47878c115ed8e488726fe5d3dff8abe429014227
Brandon Corbin <brandon@icorbin.com>
2020-04-01 13:19:55 -0400

    Trying out Weekly Ledgers instead of monthly

--- config/global.js
+++ config/global.js
@@ -1,16 +1,16 @@
 export default {
   storage_engine: "local", // local or blockstack
-  book_time_format: "YYYY-MM",
-  book_time_unit: "month",
-  data_root: "data/v01",
+  book_time_format: "YYYY-w",
+  book_time_unit: "week",
+  data_root: "v5-data",
   data_people_key: "peopleV2",
-  book_root: "data/v01/books",
+  book_root: "v5-data/books",
   tracker_file: "trackers.json",
   board_file: "boards.json",
   always_locate_key: "always-locate",
   dark_mode_key: "dark-mode",
   theme_key: "theme",
-  user_meta_path: "data/v01/meta.json",
+  user_meta_path: "v5-data/meta.json",
   support_email: "support@happydata.org",
   support_contact: "Email Brandon"
 };
