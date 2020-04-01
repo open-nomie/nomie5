0dac042dc69763fadd800551f9fad11d8beca2b5
Brandon Corbin <brandon@icorbin.com>
2020-04-01 13:21:32 -0400

    new: Adding day of week and advanced dayjs formating

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from "./App.svelte";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
+import weekOfYear from "dayjs/plugin/weekOfYear";
+dayjs.extend(weekOfYear);
+import advancedFormat from "dayjs/plugin/advancedFormat";
+dayjs.extend(advancedFormat);
 
 // Register service worker if not localhost
 if ("serviceWorker" in navigator && window.location.origin.search(/(localhost)/) == -1) {
