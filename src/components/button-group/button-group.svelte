510f381ca78dc092d9976d3329c912f8300f8a8b
Brandon Corbin <brandon@icorbin.com>
2020-04-04 17:31:53 -0400

    enhance: adding inverse option to btn group

--- src/components/button-group/button-group.svelte
+++ src/components/button-group/button-group.svelte
@@ -2,18 +2,21 @@
   import Icon from "../icon.svelte";
   export let buttons = [];
   export let size = "sm";
+  export let labelClass = "";
+  export let inverse = false;
 </script>
 
 {#if buttons.length}
-  <div class="btn-group w-100">
+  <div class="btn-group w-100 {inverse ? 'inverse' : ''}">
     {#each buttons as button, index (button.label + button.icon)}
       <button
-        class="btn {button.active ? 'active' : ''} btn-{size}"
+        class="btn {button.active ? 'active' : ''} btn-{size}
+        "
         on:click={button.click}>
         {#if button.label}
-          {button.label}
+          <div class={labelClass}>{button.label}</div>
         {:else if button.icon}
-          <i class="zmdi zmdi-{button.icon}" />
+          <i class="zmdi zmdi-{button.icon} {labelClass}" />
         {/if}
       </button>
     {/each}
