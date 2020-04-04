1630551cd3240d09fbdf6d64e53fc868768588d9
Brandon Corbin <brandon@icorbin.com>
2020-04-04 13:44:03 -0400

    enhance: Adding .escape(str) method to regex

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -3,5 +3,8 @@ export default {
   tag_v4: /(#[\x00-\xFF][^\s]+)/, //new as of 3.1.3
   tag: /(#[\x00-\xFF][^\s|'|,|.|!|?]+)/,
   person: /(\@[\x00-\xFF][^\s|'|,|.|!|?]+)/,
-  context: /(\+[\x00-\xFF][^\s|'|,|.|!|?]+)/
+  context: /(\+[\x00-\xFF][^\s|'|,|.|!|?]+)/,
+  escape(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+  }
 };
