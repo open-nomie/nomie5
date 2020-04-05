8dcef81668d708c0df3eb1de542aaa515b453b4d
Brandon Corbin <brandon@icorbin.com>
2020-04-05 16:58:51 -0400

    enhance: New V5 stats

---
+++ src/modules/stats/statsV5.js
@@ -0,0 +1,214 @@
+import dayjs from "dayjs";
+// Modules
+import Tracker from "../tracker/tracker";
+import Log from "../nomie-log/nomie-log";
+// Utils
+import Logger from "../../utils/log/log";
+import math from "../../utils/math/math";
+import regex from "../../utils/regex";
+
+export default class StatsProcessor {
+  constructor(starter = {}) {
+    this.rows = starter.rows || [];
+    this.fromDate = starter.fromDate || dayjs().subtract(1, "week");
+    this.toDate = starter.toDate || dayjs();
+    this.mode = starter.mode || "w";
+    this.tracker = starter.tracker || null;
+  }
+
+  generate(config = {}) {
+    this.rows = config.rows || this.rows;
+    this.fromDate = config.fromDate || this.fromDate;
+    this.toDate = config.toDate || this.toDate;
+    this.mode = config.mode || this.mode;
+    this.tracker = config.tracker || this.tracker;
+    return this.generateResults();
+  }
+
+  /**
+   * Generate a Value Map
+   * {
+   *    '2020-03-02': [1,2],
+   *    '2020-03-01: [1]
+   * }
+   * @param {Array} rows
+   */
+  getValueMap(rows) {
+    let valueMap = {};
+    rows.forEach((row) => {
+      if (!row.trackers) {
+        row.expand();
+      }
+      let unitFormat = this.mode == "d" ? "hh" : "YYYY-MM-DD";
+      let dayKey = dayjs(row.end).format(unitFormat);
+      valueMap[dayKey] = valueMap[dayKey] || [];
+      if (this.tracker.type == "person" || this.tracker.type == "context") {
+        valueMap[dayKey].push(1);
+      } else {
+        if (row.trackers[this.tracker.tag]) {
+          let value = row.trackers[this.tracker.tag].value;
+          value = isNaN(value) ? 0 : value;
+          valueMap[dayKey].push(value);
+        }
+      }
+    });
+    console.log("valueMap", valueMap);
+    return valueMap;
+  }
+
+  /**
+   * Get the Min and Max values
+   * from a valueMap
+   * @param {Object} valueMap
+   */
+  getMinMaxFromValueMap(valueMap) {
+    let min = { value: null, dateKey: null, date: null };
+    let max = { value: null, dateKey: null, date: null };
+    let valueArray = Object.keys(valueMap)
+      .map((dateKey) => {
+        let value;
+
+        if (this.getMath() === "sum") {
+          value = math.sum(valueMap[dateKey]);
+        } else {
+          value = math.average(valueMap[dateKey]);
+        }
+        return {
+          dateKey,
+          value,
+          date: dayjs(dateKey),
+        };
+      })
+      .sort((a, b) => {
+        return a.value < b.value ? -1 : 1;
+      });
+
+    if (valueArray.length) {
+      min = valueArray[0];
+      max = valueArray[valueArray.length - 1];
+    }
+    return { min, max };
+  }
+
+  // Generate Chart Data
+  getChartDataByType(unit, timeFormat, labelFormat, valueMapTotals) {
+    let labels = [];
+    let values = [];
+    let unitValues = valueMapTotals.days;
+
+    let from = unit == "hour" ? this.fromDate.startOf("day") : this.fromDate;
+    let to = unit == "hour" ? this.fromDate.endOf("day") : this.toDate;
+    let diff = to.diff(from, unit);
+
+    for (var i = 0; i < diff; i++) {
+      const unitDate = dayjs(from).add(i, unit);
+      let key = unitDate.format(timeFormat);
+      let label = unitDate.format(labelFormat);
+      if (unitValues.hasOwnProperty(key)) {
+        const value = this.getMath() == "sum" ? unitValues[key].sum : unitValues[key].avg;
+        labels.push({ x: label });
+        values.push({ x: label, y: value });
+      } else {
+        labels.push({ x: label });
+        values.push({ x: label, y: 0 });
+      }
+    }
+
+    console.log("getChartDataByType", { diff, timeFormat, labelFormat, valueMapTotals });
+    return {
+      labels,
+      values,
+    };
+  }
+
+  getChartData(valueMapTotals) {
+    if (this.mode == "d") {
+      let { labels, values } = this.getChartDataByType("hour", "H", "H", valueMapTotals);
+      return {
+        mode: this.mode,
+        labels,
+        values,
+      };
+    } else if (this.mode == "w" || this.mode == "m") {
+      let { labels, values } = this.getChartDataByType("day", "YYYY-MM-DD", "Do", valueMapTotals);
+      return {
+        mode: this.mode,
+        labels,
+        values,
+      };
+    } else if (this.mode == "y") {
+      let { labels, values } = this.getChartDataByType("month", "YYYY-MM", "MMM", valueMapTotals);
+      return {
+        mode: this.mode,
+        labels,
+        values,
+      };
+    }
+  } // end to Chart Data;
+
+  getValueMapTotals(valueMap) {
+    let newMap = {
+      sum: 0,
+      avg: 0,
+      days: {
+        ...valueMap, // put the valuemap in this new map
+      },
+    };
+    // Hold all values for total sum and avg
+    let allValues = [];
+    // Loop over the days provided
+    Object.keys(newMap.days).forEach((date) => {
+      let values = newMap.days[date];
+      // If we should ignore zeros, then
+      // filter them out.
+      let ignoreZeros = !this.tracker ? true : this.tracker.ignore_zeros;
+      if (ignoreZeros) {
+        values = values.filter((v) => {
+          return v !== 0 ? true : false;
+        });
+      }
+      // Let's calcuate the days total
+      if (values.length) {
+        // If it's sum - add them all up
+        if (this.getMath() === "sum") {
+          allValues.push(math.sum(values));
+        } else {
+          // Else add it to the array for average lating
+          allValues = [...allValues, ...values];
+        }
+      }
+      // Sum and Avg this day
+      newMap.days[date] = {
+        sum: math.sum(values),
+        avg: math.average(values),
+      };
+    }); // end loop over each day
+
+    newMap.sum = math.sum(allValues);
+    newMap.avg = math.average(allValues);
+
+    return newMap;
+  }
+
+  getMath() {
+    return this.tracker.math || "sum";
+  }
+
+  generateResults() {
+    let valueMap = this.getValueMap(this.rows);
+    let valueMapTotals = this.getValueMapTotals(valueMap);
+    let minMax = this.getMinMaxFromValueMap(valueMap);
+    let chart = this.getChartData(valueMapTotals);
+    return {
+      tracker: this.tracker,
+      type: this.tracker.type,
+      math: this.getMath(),
+      rows: this.rows,
+      chart: chart,
+      avg: valueMapTotals.avg,
+      sum: valueMapTotals.sum,
+      min: minMax.min,
+      max: minMax.max,
+    };
+  }
+}
