20d84ecf67ca9dbfed25ab3aebf53271c552b1e1
Brandon Corbin <brandon@icorbin.com>
2020-04-05 16:59:20 -0400

    enhance: Regex now has a template function for returning regex for username, context and tag

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,10 +1,22 @@
-export default {
+const regex = {
   tag_v311: /(#[^\s|\.|#|\?)][\x00-\xFF][^\s]+)/,
   tag_v4: /(#[\x00-\xFF][^\s]+)/, //new as of 3.1.3
-  tag: /(#[\x00-\xFF][^\s|'|,|.|!|?]+)/,
+  tag: /(#[\x00-\xFF][^\s|'|,|!|?]+)/,
   person: /(\@[\x00-\xFF][^\s|'|,|.|!|?]+)/,
   context: /(\+[\x00-\xFF][^\s|'|,|.|!|?]+)/,
   escape(str) {
     return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
-  }
+  },
+  template: {
+    person(name) {
+      return new RegExp(`(\@${regex.escape(name)}[^\s|'|,|.|!|?]+)`, "gi");
+    },
+    tag(tag) {
+      return new RegExp(`(\#${regex.escape(tag)}[^\s|'|,|!|?]+)`, "gi");
+    },
+    context(context) {
+      return new RegExp(`(\+${regex.escape(context)}[^\s|'|,|.|!|?]+)`, "gi");
+    },
+  },
 };
+export default regex;
