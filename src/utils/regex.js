4c306d9ee58622de82e6e52865b67ea9b4c29a3f
Brandon Corbin <brandon@icorbin.com>
2020-04-06 18:33:12 -0400

    fix: account for ' in regexes

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,9 +1,9 @@
 const regex = {
   tag_v311: /(#[^\s|\.|#|\?)][\x00-\xFF][^\s]+)/,
   tag_v4: /(#[\x00-\xFF][^\s]+)/, //new as of 3.1.3
-  tag: /(#[\x00-\xFF][^\s|'|,|!|?]+)/,
-  person: /(\@[\x00-\xFF][^\s|'|,|.|!|?]+)/,
-  context: /(\+[\x00-\xFF][^\s|'|,|.|!|?]+)/,
+  tag: /(#[\x00-\xFF][^\s|'|,|!|’|?]+)/,
+  person: /(\@[\x00-\xFF][^\s|'|,|.|!|’|?]+)/,
+  context: /(\+[\x00-\xFF][^\s|'|,|.|!|’|?]+)/,
   escape(str) {
     return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
   },
