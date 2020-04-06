582e3e24719ab5ebe6be140f72a8f9f8d7dd65ec
Brandon Corbin <brandon@icorbin.com>
2020-04-05 23:10:55 -0400

    add: Created new Positivity Selector

---
+++ src/components/positivity-selector/positivity-selector.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import NButtonGroup from "../button-group/button-group.svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let score = 0;
+  export let className = "";
+  export let style = "";
+  export let size = "md";
+
+  function onChange() {
+    dispatch("change", score);
+  }
+</script>
+
+<NButtonGroup
+  inverse
+  {style}
+  {className}
+  labelClass="text-{size}"
+  buttons={[{ label: '😩', active: score === -2, click: () => {
+        score = -2;
+        onChange();
+      } }, { label: '😞', active: score === -1, click: () => {
+        score = -1;
+        onChange();
+      } }, { label: '😐', active: score === 0, click: () => {
+        score = 0;
+        onChange();
+      } }, { label: '🙂', active: score === 1, click: () => {
+        score = 1;
+        onChange();
+      } }, { label: '😁', active: score === 2, click: () => {
+        score = 2;
+        onChange();
+      } }]} />
