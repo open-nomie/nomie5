938334a611d28ea0538b276d4480fc15414a6d4c
Brandon Corbin <brandon@icorbin.com>
2020-04-08 22:26:18 -0400

    enhance: ui fixes on check-in

--- src/containers/people/person-check-in.svelte
+++ src/containers/people/person-check-in.svelte
@@ -1,16 +1,25 @@
 <script>
-  import { Interact } from "../../store/interact.js";
-  import { Lang } from "../../store/lang.js";
-  import { LedgerStore } from "../../store/ledger.js";
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  // Modules
   import NomieLog from "../../modules/nomie-log/nomie-log";
+
+  // Components
   import Spinner from "../../components/spinner/spinner.svelte";
   import NItem from "../../components/list-item/list-item.svelte";
   import AutoComplete from "../../components/auto-complete/auto-complete.svelte";
+  import NIcon from "../../components/icon/icon.svelte";
   import NButtonGroup from "../../components/button-group/button-group.svelte";
   import NPositivitySelector from "../../components/positivity-selector/positivity-selector.svelte";
-  import { createEventDispatcher } from "svelte";
 
-  const dispatch = createEventDispatcher();
+  // utils
+  import tick from "../../utils/tick/tick";
+
+  // Stores
+  import { Interact } from "../../store/interact.js";
+  import { Lang } from "../../store/lang.js";
+  import { LedgerStore } from "../../store/ledger.js";
 
   const state = {
     note: ` @${$Interact.people.active} `,
@@ -31,7 +40,8 @@
       let saved = await LedgerStore.saveLog(log);
       state.checkingIn = false;
       state.checkedIn = true;
-      dispatch("checkedIn");
+      await tick(600);
+      dispatch("checkedIn", saved);
     } catch (e) {
       Interact.alert("Error", e.message);
       state.checkingIn = false;
@@ -63,20 +73,7 @@
       on:change={evt => {
         state.score = evt.detail;
       }} />
-    <!-- <NButtonGroup
-      inverse
-      labelClass="text-xl"
-      buttons={[{ label: '😩', active: state.score === -2, click: () => {
-            state.score = -2;
-          } }, { label: '😞', active: state.score === -1, click: () => {
-            state.score = -1;
-          } }, { label: '😐', active: state.score === 0, click: () => {
-            state.score = 0;
-          } }, { label: '🙂', active: state.score === 1, click: () => {
-            state.score = 1;
-          } }, { label: '😁', active: state.score === 2, click: () => {
-            state.score = 2;
-          } }]} /> -->
+
   </div>
   {#if !state.checkingIn && !state.checkedIn}
     <button class="btn btn-block btn-primary mt-4" on:click={checkIn}>
@@ -87,9 +84,9 @@
       <Spinner size={24} className="mr-3" />
       <div class="ml-2">Checking In...</div>
     </button>
-  {:else if state.checkedIn}
-    <button class="btn btn-block btn-green mt-4">
-      <span class="zmdi zmdi-check text-white" />
-    </button>
   {/if}
+  <div
+    class="text-center mt-4 animate up {state.checkedIn ? 'visible' : 'hidden'}">
+    <NIcon name="checkmarkOutline" size="60" className="fill-green" />
+  </div>
 </div>
