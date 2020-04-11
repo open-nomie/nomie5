1a1d34b98c7e430289de589e2f7494482362c94e
Brandon Corbin <brandon@icorbin.com>
2020-04-10 20:37:59 -0400

    Update snake-case.js

--- src/utils/snake-case/snake-case.js
+++ src/utils/snake-case/snake-case.js
@@ -1,6 +1,6 @@
-export default str => {
+export default (str) => {
   return str
     .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-    .map(x => x.toLowerCase())
-    .join("_");
+    .join("_")
+    .toLowerCase();
 };
