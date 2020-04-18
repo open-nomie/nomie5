c677f673d25dedbb570641c3cf189f9dc25630a7
Brandon Corbin <brandon@icorbin.com>
2020-04-17 20:05:03 -0400

    fix: removing material design icons from service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -23,38 +23,33 @@ const PRECACHE_URLS = [
   "manifest.json",
   "favicon.png",
   "images/icons/nomie-android-192.png",
-  "/fonts/Material-Design-Iconic-Font.eot",
-  "/fonts/Material-Design-Iconic-Font.svg",
-  "/fonts/Material-Design-Iconic-Font.ttf",
-  "/fonts/Material-Design-Iconic-Font.woff",
-  "/fonts/Material-Design-Iconic-Font.woff2",
-  "./" // Alias for index.html
+  "./", // Alias for index.html
 ];
 
 // The install handler takes care of precaching the resources we always need.
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches
       .open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then((cache) => cache.addAll(PRECACHE_URLS))
       .then(self.skipWaiting())
   );
 });
 
 // The activate handler takes care of cleaning up old caches.
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   const currentCaches = [PRECACHE, RUNTIME];
   console.log("Nomie WS: Activated");
   event.waitUntil(
     caches
       .keys()
-      .then(cacheNames => {
+      .then((cacheNames) => {
         console.log("Nomie WS: Caches", cacheNames);
-        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
       })
-      .then(cachesToDelete => {
+      .then((cachesToDelete) => {
         return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
+          cachesToDelete.map((cacheToDelete) => {
             return caches.delete(cacheToDelete);
           })
         );
@@ -66,16 +61,16 @@ self.addEventListener("activate", event => {
 // The fetch handler serves responses for same-origin resources from a cache.
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
+      caches.match(event.request).then((cachedResponse) => {
         if (cachedResponse) {
           return cachedResponse;
         }
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
             // Put a copy of the response in the runtime cache.
             return cache.put(event.request, response.clone()).then(() => {
               return response;
