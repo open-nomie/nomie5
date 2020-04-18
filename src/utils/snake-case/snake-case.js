565038ce60d808721fde9832e3902feb70c13789
Brandon Corbin <brandon@icorbin.com>
2020-04-18 16:22:14 -0400

    fix: error caused by padding null

--- src/utils/snake-case/snake-case.js
+++ src/utils/snake-case/snake-case.js
@@ -1,6 +1,8 @@
 export default (str) => {
-  return str
-    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-    .join("_")
-    .toLowerCase();
+  const matches = str.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g);
+  if (matches) {
+    return matches.join("_").toLowerCase();
+  } else {
+    return "";
+  }
 };
