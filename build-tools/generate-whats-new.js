07b6a070cb3d8c20a43f81e962d784da0b99875d
Brandon Corbin <brandon@icorbin.com>
2020-06-13 08:38:39 -0400

    chore: adding stickiness to the what's new generator

--- build-tools/generate-whats-new.js
+++ build-tools/generate-whats-new.js
@@ -5,22 +5,37 @@ const options = {
   repo: ROOT,
 };
 
+let current = fs.readFileSync(`${ROOT}/src-data/whatsNew.json`, "utf-8");
+let pkgRaw = fs.readFileSync(`${ROOT}/package.json`, "utf-8") || "{}";
+let pkg = JSON.parse(pkgRaw);
+let version = pkg.version;
+
 const commits = gitlog(options);
 
-let features = [];
-let fixes = [];
-let chores = [];
+let features = current.features || [];
+let fixes = current.fixes || [];
+let chores = current.chores || [];
+let build_date = new Date();
 
 commits.forEach((commit) => {
   if (commit.subject && commit.subject.search("fix:") > -1) {
-    fixes.push(commit.subject.replace("fix: ", ""));
+    let title = commit.subject.replace("fix: ", "");
+    if (!fixes.find((f) => f.version == version && f.title == title)) {
+      fixes.push({ version, title });
+    }
   }
   if (commit.subject && commit.subject.search("feat:") > -1) {
-    features.push(commit.subject.replace("feat: ", ""));
+    let title = commit.subject.replace("feat: ", "");
+    if (!features.find((f) => f.version == version && f.title == title)) {
+      features.push({ version, title });
+    }
   }
   if (commit.subject && commit.subject.search("chore:") > -1) {
-    chores.push(commit.subject.replace("chore: ", ""));
+    let title = commit.subject.replace("chore: ", "");
+    if (!chores.find((f) => f.version == version && f.title == title)) {
+      chores.push({ version, title });
+    }
   }
 });
 
-fs.writeFileSync(`${ROOT}/src-data/whatsNew.json`, JSON.stringify({ features, fixes, chores }));
+fs.writeFileSync(`${ROOT}/src-data/whatsNew.json`, JSON.stringify({ features, fixes, chores, build_date }));
