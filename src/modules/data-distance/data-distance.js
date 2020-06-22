1101f3c3c3c1b11afc4e790f247304d3c4d51e88
Brandon Corbin <brandon@icorbin.com>
2020-06-22 19:18:09 -0400

    feat: adding r-squared to "pick it for me"

--- src/modules/data-distance/data-distance.js
+++ src/modules/data-distance/data-distance.js
@@ -1,6 +1,30 @@
 import euclidean from "euclidean-distance";
 import math from "../../utils/math/math";
+import rSquared from "r-squared";
 // import DTW from "dynamic-time-warping";
+
+function dataPoints(arr) {
+  return arr.filter((a) => a).length;
+}
+
+const rS = (ar1, ar2) => {
+  let ar1DP = dataPoints(ar1);
+  let ar2DP = dataPoints(ar2);
+
+  return new Promise((resolve, reject) => {
+    if (ar2DP < ar1DP * 0.3) {
+      resolve(0);
+    } else {
+      rSquared.rSquared(ar1, ar2, (err, data) => {
+        if (!err) {
+          resolve(data);
+        } else {
+          reject(err);
+        }
+      });
+    }
+  });
+};
 
 const euclideanDistance = (arr1, arr2) => {
   let array1 = math.percentile(arr1);
@@ -20,4 +44,5 @@ const dtwDistance = (arr1, arr2) => {
 export default {
   dtw: dtwDistance,
   euclidean: euclideanDistance,
+  score: rS,
 };
