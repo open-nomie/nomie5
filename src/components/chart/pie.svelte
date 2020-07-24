52e104ffbc76b14a6483aeedb6fcb4aa9b398c94
Brandon Corbin <brandon@icorbin.com>
2020-07-24 17:59:03 -0400

    feat: adding chart.js for better charting

---
+++ src/components/chart/pie.svelte
@@ -0,0 +1,48 @@
+<script type="ts">
+  import { onMount } from "svelte";
+  import Chart from "chart.js";
+  import nid from "../../modules/nid/nid";
+
+  export let data: Array<IPieData> = [];
+  export let size: any = "100";
+
+  const chartId = `chart-${nid()}`;
+
+  interface IPieData {
+    label: string;
+    value: number;
+    color: string;
+  }
+
+  function createDoughnut() {
+    var ctx = document.getElementById(chartId);
+    let myChart = new Chart(ctx, {
+      type: "doughnut",
+      data: {
+        labels: data.map((row) => row.label),
+        datasets: [
+          {
+            backgroundColor: data.map((row) => row.color),
+            data: data.map((row) => row.value),
+          },
+        ],
+      },
+      options: {
+        legend: {
+          display: false,
+        },
+        maintainAspectRatio: false,
+        title: {
+          display: false,
+        },
+      },
+    });
+  }
+  onMount(createDoughnut);
+</script>
+
+<style>
+
+</style>
+
+<canvas id={chartId} width="150" height="150" />
