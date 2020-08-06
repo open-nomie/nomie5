b7be76324618c6798a8831495b821b70f2d09cb8
Brandon Corbin <brandon@icorbin.com>
2020-08-06 18:49:13 -0400

    style: adding some padding to people bar

--- src/routes/people.svelte
+++ src/routes/people.svelte
@@ -27,10 +27,10 @@
     view: "time",
     stats: {},
     searchTerm: null,
-    initialized: false
+    initialized: false,
   };
 
-  const personClicked = username => {
+  const personClicked = (username) => {
     Interact.person(username);
   };
 
@@ -44,9 +44,7 @@
   }
 
   function loadPeople() {
-    const longTimeAgo = dayjs()
-      .subtract(100, "years")
-      .toDate();
+    const longTimeAgo = dayjs().subtract(100, "years").toDate();
 
     state.people = getPeople().sort((a, b) => {
       return $PeopleStore.people[a].last < $PeopleStore.people[b].last ? 1 : -1;
@@ -67,9 +65,7 @@
 
   async function addPerson() {
     try {
-      let username = await Interact.prompt(
-        Lang.t("people.person-name", "Person Name")
-      );
+      let username = await Interact.prompt(Lang.t("people.person-name", "Person Name"));
       if (username) {
         let person = await PeopleStore.addByName(username);
         if (person) {
@@ -85,7 +81,7 @@
   function getPeople() {
     // The $PeopleStore.peple is a map - username is the key
     if (state.searchTerm) {
-      return Object.keys(($PeopleStore || {}).people || {}).filter(person => {
+      return Object.keys(($PeopleStore || {}).people || {}).filter((person) => {
         return person.toLowerCase().search(state.searchTerm) > -1;
       });
     } else {
@@ -105,11 +101,7 @@
 
 <NLayout pageTitle="People">
   <div slot="header">
-    <NSearchBar
-      on:change={searchPeople}
-      on:clear={clearSearch}
-      placeholder="Search People..."
-      autocomplete>
+    <NSearchBar on:change={searchPeople} on:clear={clearSearch} placeholder="Search People..." autocomplete>
       <button on:click={addPerson} slot="right" class="btn btn-icon btn-clear">
         <NIcon name="userAdd" className="fill-primary-bright" />
       </button>
@@ -121,15 +113,11 @@
     <div class="n-list my-2">
       {#if !state.people.length && !state.searchTerm && state.initialized}
         <NItem className=" py-3 bg-transparent">
-          <div class="text-md text-center">
-            Track and monitor how you interact with your friends and family.
-          </div>
+          <div class="text-md text-center">Track and monitor how you interact with your friends and family.</div>
           <div class="text-sm mt-2 text-center">
             <span class="fake-link" on:click={addPerson}>Add a person</span>
             or
-            <span class="fake-link" on:click={PeopleStore.searchForPeople}>
-              Find recent @people
-            </span>
+            <span class="fake-link" on:click={PeopleStore.searchForPeople}>Find recent @people</span>
           </div>
 
         </NItem>
@@ -142,36 +130,23 @@
       {#each state.people as person}
         <NItem
           truncate
-          className="pt-1 pb-1 clickable solo box-shadow mb-3 mr-0 filler"
+          className="py-2 clickable solo box-shadow mb-3 mr-0 filler"
           on:click={() => {
             personClicked(person);
           }}>
           <div slot="left">
             <button class="btn btn-clear tap-icon p-1 mr-1">
-              <NIcon
-                name="addOutline"
-                className="fill-primary-bright"
-                size={18} />
+              <NIcon name="addOutline" className="fill-primary-bright" size={18} />
             </button>
             {#if $PeopleStore.people[person].avatar}
-              <AvatarBall
-                size={48}
-                avatar={$PeopleStore.people[person].avatar}
-                style={`border-radius:32%; overflow:hidden`} />
+              <AvatarBall size={48} avatar={$PeopleStore.people[person].avatar} style={`border-radius:32%; overflow:hidden`} />
             {:else if $PeopleStore.people[person].displayName}
-              <AvatarBall
-                size={48}
-                username={$PeopleStore.people[person].displayName}
-                style={`border-radius:32%; overflow:hidden`} />
+              <AvatarBall size={48} username={$PeopleStore.people[person].displayName} style={`border-radius:32%; overflow:hidden`} />
             {/if}
           </div>
-          <div class="title truncate w-100">
-            {$PeopleStore.people[person].displayName}
-          </div>
+          <div class="title truncate w-100">{$PeopleStore.people[person].displayName}</div>
           {#if $PeopleStore.people[person].last}
-            <div class="note">
-              {dayjs($PeopleStore.people[person].last).fromNow()}
-            </div>
+            <div class="note">{dayjs($PeopleStore.people[person].last).fromNow()}</div>
           {/if}
           <div slot="right" class="n-row">
 
@@ -187,9 +162,7 @@
       {/each}
       {#if state.people.length}
         <div class="p-2 text-center">
-          <span class="fake-link" on:click={PeopleStore.searchForPeople}>
-            Find recent @people
-          </span>
+          <span class="fake-link" on:click={PeopleStore.searchForPeople}>Find recent @people</span>
         </div>
       {/if}
     </div>
