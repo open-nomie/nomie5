40cc91b2ef00967fbf37fbd4f20293b73c8207e4
Brandon Corbin <brandon@icorbin.com>
2020-08-11 15:35:17 -0400

    chore: cleanup

--- src/modules/tracker/tracker-inputer.ts
+++ src/modules/tracker/tracker-inputer.ts
@@ -58,7 +58,6 @@ export default class TrackerInputer {
 
   async getTrackerInputAsString(tracker: TrackerConfig, value?: number, allowSave: boolean = false): Promise<string> {
     const response = await Interact.trackerInput(tracker, { value, allowSave });
-    console.log({ response });
     if (response && response.tracker) {
       return `#${response.tracker.tag}(${response.value}) ${response.suffix || ""}`;
     } else {
