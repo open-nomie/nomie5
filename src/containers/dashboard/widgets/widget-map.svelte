e61ad3c2db5a30071b8eaa1b714f9e2e41b042a5
Brandon Corbin <brandon@icorbin.com>
2020-08-18 14:58:47 -0400

    feat: adding map dashboard widget

---
+++ src/containers/dashboard/widgets/widget-map.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { Widget } from "../../../modules/dashboard/widget";
+  import Map from "../../map/map.svelte";
+  export let widget: Widget;
+</script>
+
+{#if widget}
+  <div class="map-value" style="width:100%;">
+    <Map height={190} records={widget.stats.rows} />
+  </div>
+{/if}
