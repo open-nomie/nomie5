96a3f6604cbc007af3dadbfd3c0db37e5d06634d
Brandon Corbin <brandon@icorbin.com>
2020-09-13 15:38:45 -0400

    chore: artifacts

--- build-tools/csv-generator.js
+++ build-tools/csv-generator.js
@@ -2,16 +2,17 @@ const dayjs = require("dayjs");
 const fs = require("fs");
 const _ = require("lodash");
 
-let daycount = 60;
+let daycount = 800;
 let tag = "mood";
+let tag2 = "sleep_quality";
 
 let rows = [["date", "note"]];
 
 let start = dayjs().subtract(daycount, "day");
 for (var i = 0; i < daycount; i++) {
   let d = start.add(i, "day");
-
   rows.push([d.hour(_.random(1, 23)).minute(_.random(1, 23)).toJSON(), `#${tag}(${i + 1})`]);
+  rows.push([d.hour(_.random(8, 12)).minute(_.random(1, 23)).toJSON(), `#${tag2}(${_.random(20, 90)})`]);
 }
 
 let csv = rows
