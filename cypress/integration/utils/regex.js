0ba94cd1f8331d9b439e93821525021ade497a65
Brandon Corbin <brandon@icorbin.com>
2020-10-12 18:36:49 -0400

    refactor: delete old tests

--- cypress/integration/utils/regex.js
+++
@@ -1,7 +0,0 @@
-import regexs from "../../../src/utils/regex";
-
-describe("utils/regex", function () {
-  it("escapes terms", () => {
-    expect(regexs.escape("54?")).to.equal("54\\?");
-  });
-});
