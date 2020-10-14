2831f5b37ec202dc331156d35323861c23696aa8
Brandon Corbin <brandon@icorbin.com>
2020-10-14 09:42:26 -0400

    chore: created new list component

---
+++ src/components/list/list.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let style: string = "";
+  export let solo: boolean = false;
+  export let transparent: boolean = false;
+  export let className: string = "";
+</script>
+
+<div class="n-list {solo ? 'solo' : ''} {transparent ? 'transparent' : ''} {className}" {style}>
+  <slot />
+</div>
