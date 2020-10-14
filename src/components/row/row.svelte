f280432d6574ccdd3f2692f72a486ddd1d05b9a7
Brandon Corbin <brandon@icorbin.com>
2020-10-14 11:06:29 -0400

    chore: adding new row and spacer components

---
+++ src/components/row/row.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let className = "";
+  export let style = "";
+</script>
+
+<div class="n-row {className}" {style}>
+  <slot />
+</div>
--- src/components/spacer/spacer.svelte
+++ src/components/spacer/spacer.svelte
@@ -1,1 +1,6 @@
-<div class="filler" />
+<script lang="ts">
+  export let className = "";
+  export let noFill: boolean = false;
+</script>
+
+<div class="{!noFill ? 'filler' : 'no-fill'} {className}" />
