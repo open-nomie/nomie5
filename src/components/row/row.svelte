576a749a94e1490633cc6cb6d31e2ad2d20a4422
Brandon Corbin <brandon@icorbin.com>
2020-10-17 10:14:21 -0400

    style: adding left right inset on rows

--- src/components/row/row.svelte
+++ src/components/row/row.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
   export let className = "";
   export let style = "";
+  export let insetLeft: boolean = false;
+  export let insetRight: boolean = false;
 </script>
 
-<div class="n-row {className}" {style}>
+<style>
+  .inset-left {
+    margin-left: -12px;
+  }
+  .inset-right {
+    margin-right: -12px;
+  }
+</style>
+
+<div class="n-row {className} {insetRight ? 'inset-right' : ''} {insetLeft ? 'inset-left' : ''}" {style}>
   <slot />
 </div>
