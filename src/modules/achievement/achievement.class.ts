7d911ec277f8a35e0459025fbd02ab2cf059f8ec
Brandon Corbin <brandon@icorbin.com>
2020-10-25 08:34:45 -0400

    chore: fixing formatting

--- src/modules/achievement/achievement.class.ts
+++ src/modules/achievement/achievement.class.ts
@@ -1,10 +1,10 @@
 import nid from "../nid/nid";
-import TrackableElement from "../trackable-element/trackable-element";
+import type TrackableElement from "../trackable-element/trackable-element";
 import type { IConditionIsUnit } from "../../modules/scoring/score-tracker";
 
 export type AchTimeframe = "day" | "week" | "month";
 export interface AchConditionConfig {
-  element?:TrackableElement;
+  element?: TrackableElement;
   condition?: IConditionIsUnit;
   value?: number;
 }
@@ -15,26 +15,26 @@ export class AchCondition {
   value?: number;
   constructor(starter: AchConditionConfig) {
     this.value = starter.value;
-    this.condition = starter.condition
+    this.condition = starter.condition;
     this.element = starter.element;
   }
 }
 
 export interface AchievementConfig {
   id?: string;
-  timeframe?: AchTimeframe,
-  conditions?: Array<AchCondition>
+  timeframe?: AchTimeframe;
+  conditions?: Array<AchCondition>;
 }
 
 export default class Achievement {
   id?: string;
-  timeframe?: AchTimeframe,
-  conditions?: Array<AchCondition>
+  timeframe?: AchTimeframe;
+  conditions?: Array<AchCondition>;
   constructor(starter: AchievementConfig = {}) {
     this.id = starter.id || `ach-${nid(6)}`;
     this.timeframe = starter.timeframe || "day";
-    this.conditions = (starter.conditions ? starter.conditions : []).map((cond)=>{
-      return new AchCondition(cond)
-    })
+    this.conditions = (starter.conditions ? starter.conditions : []).map((cond) => {
+      return new AchCondition(cond);
+    });
   }
 }
