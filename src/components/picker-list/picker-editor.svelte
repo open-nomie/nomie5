53565bfc1985d1da7139bbce0c9c14b6684c28af
Brandon Corbin <brandon@icorbin.com>
2020-12-02 15:51:53 -0500

    chore: picker work

---
+++ src/components/picker-list/picker-editor.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import NItem from "../list-item/list-item.svelte";
+
+  import { createEventDispatcher } from "svelte";
+  import Input from "../input/input.svelte";
+  import { Lang } from "../../store/lang";
+  import Text from "../text/text.svelte";
+  const dispatch = createEventDispatcher();
+
+  export let style = "";
+  export let className = "";
+  export let itemClass = "";
+  export let list: Array<any> = [];
+  // export let editWithText = true;
+  // export let mode = "select";
+  // export let tracker: ITracker;
+  export let showHeaderContent: boolean = true;
+  // export let showSaveEditButton: boolean = true;
+
+  // export let canSelect: boolean = true;
+
+  let ready = false;
+  let textList;
+  let picks = [];
+
+  function textListChanged(evt) {
+    picks = textList.getValue().split("\n");
+    dispatch("change", picks);
+  }
+
+  $: if (list && list.length && picks.length == 0) {
+    picks = list || [];
+  }
+
+  onMount(() => {
+    ready = true;
+  });
+</script>
+
+<style>
+  .n-picker-list.edit-mode {
+    max-width: 100%;
+    position: relative;
+    align-items: start;
+    flex-grow: 1;
+    box-shadow: var(--box-shadow-float);
+  }
+</style>
+
+<div class="n-picker-list edit-mode {className}" {style}>
+  {#if showHeaderContent}
+    <NItem title={Lang.t('picker.title', 'List one item per line')} className={itemClass} bg="transparent">
+      <Text size="sm" faded>Include #trackers @people +context. Add a : to make a Title:</Text>
+    </NItem>
+  {/if}
+  {#if ready}
+    <Input
+      solo
+      listItem
+      type="textarea"
+      value={picks.join('\n')}
+      bind:this={textList}
+      on:change={textListChanged}
+      inputStyle="font-weight:bold; height:45vh; line-height:28px" />
+    <div class="filler" />
+  {/if}
+</div>
--- src/containers/tracker/input/picker.svelte
+++ src/containers/tracker/input/picker.svelte
@@ -1,13 +1,33 @@
-<script>
+<script lang="ts">
   import { createEventDispatcher } from "svelte";
-  import PickerList from "../../../components/picker-list/picker-list.svelte";
+  import Button from "../../../components/button/button.svelte";
+  import Icon from "../../../components/icon/icon.svelte";
+  import PickerListEditor from "../../../components/picker-list/picker-editor.svelte";
+  import PickerSelect from "../../../components/picker-list/picker-select.svelte";
+  import type TrackerConfig from "../../../modules/tracker/tracker";
+  import { Lang } from "../../../store/lang";
+  import { TrackerStore } from "../../../store/tracker-store";
 
   const dispatch = createEventDispatcher();
 
-  export let tracker;
+  export let tracker: TrackerConfig;
+
+  let selected = [];
+  let mode: "view" | "edit" = "view";
 
   function fireChange(evt) {
     dispatch("change", evt.detail);
+  }
+
+  function toggleMode() {
+    if (mode === "edit") {
+      TrackerStore.saveTracker(tracker);
+      mode = "view";
+      dispatch("enterView");
+    } else {
+      dispatch("enterEdit");
+      mode = "edit";
+    }
   }
 </script>
 
@@ -15,9 +35,35 @@
   .tracker-input.picker {
     width: 100%;
     justify-content: center;
+    align-items: flex-start;
+    position: relative;
+  }
+  :global(.picker-toggle) {
+    position: absolute !important;
+    top: 10px;
+    right: 10px;
+    z-index: 2000;
   }
 </style>
 
 <div class="tracker-input picker">
-  <PickerList {tracker} on:change={fireChange} showHeaderContent={false} />
+  <Button shape="circle" type={mode == 'view' ? 'outline' : 'solid'} color="primary" className="picker-toggle" on:click={toggleMode}>
+    {#if mode == 'view'}
+      <Icon name="edit" size="16" />
+    {:else}
+      <Icon name="checkmark" size="16" />
+    {/if}
+  </Button>
+  {#if mode == 'edit'}
+    <PickerListEditor
+      className="mx-3 mt-3"
+      list={tracker.picks}
+      on:change={(evt) => {
+        tracker.picks = evt.detail;
+      }}
+      showHeaderContent={false} />
+  {:else}
+    <PickerSelect {tracker} active={selected} on:change={fireChange} />
+  {/if}
+
 </div>
